8de621c970bbc3f16945d5c97d427afd25207d8b
greenorangeCN <42231810@qq.com>
2014-06-25 09:22:50 +0800

    Initial Commit

---
+++ NewWorld/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  NewWorld
+//
+//  Created by Seven on 14-6-25.
+//  Copyright (c) 2014年 Seven. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
